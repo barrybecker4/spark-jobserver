4c7f5552a4bd946b3478a7fc28bdaeaef1e1f845
barrybecker4 <barrybecker4@yahoo.com>
2016-12-04 06:42:23 -0800

    #570 upgrade to spark 2.0.2. Use hadoop 2.6 instead of 2.7 as that matches our version used with spark.

--- ci/install-spark.sh
+++ ci/install-spark.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -o /tmp/spark.tgz http://apache.mirror.anlx.net/spark/spark-2.0.1/spark-2.0.1-bin-hadoop2.7.tgz
+curl -o /tmp/spark.tgz http://apache.mirror.anlx.net/spark/spark-2.0.2/spark-2.0.2-bin-hadoop2.6.tgz
 tar -xvzf /tmp/spark.tgz -C /tmp
