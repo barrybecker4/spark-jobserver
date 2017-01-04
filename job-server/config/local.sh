a1fa27472089b9033075ce6c832bd59b8c33d97d
barrybecker <bbe@esi-group.com>
2017-01-04 13:55:28 -0800

    Add typical local.conf and local.sh config files for mineset so we do not need to keep creating them.

---
+++ job-server/config/local.sh
@@ -0,0 +1,28 @@
+# Environment and deploy file
+# For use with bin/server_deploy, bin/server_package etc.
+DEPLOY_HOSTS="hostname1.net
+              hostname2.net"
+
+APP_USER=mineset
+APP_GROUP=mineset
+JMX_PORT=9999
+# optional SSH Key to login to deploy server
+#SSH_KEY=/path/to/keyfile.pem
+INSTALL_DIR=$SPARK_JOBSERVER_HOME
+LOG_DIR=/var/log/job-server
+PIDFILE=spark-jobserver.pid
+JOBSERVER_MEMORY=5G
+SPARK_VERSION=1.6.3
+MAX_DIRECT_MEMORY=1G
+SPARK_CONF_DIR=$SPARK_HOME/conf
+# Only needed for Mesos deploys
+SPARK_EXECUTOR_URI=/home/spark/spark-1.6.3.tar.gz
+# Only needed for YARN running outside of the cluster
+# You will need to COPY these files from your cluster to the remote machine
+# Normally these are kept on the cluster in /etc/hadoop/conf
+# YARN_CONF_DIR=/pathToRemoteConf/conf
+# HADOOP_CONF_DIR=/pathToRemoteConf/conf
+#
+# Also optional: extra JVM args for spark-submit
+# export SPARK_SUBMIT_OPTS+="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5433"
+SCALA_VERSION=2.11.6
