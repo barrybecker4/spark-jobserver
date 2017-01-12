8fd1b661ba18478ab8256ef4d4a3e128550eaa60
barrybecker <bbe@esi-group.com>
2017-01-12 07:21:04 -0800

    Update config files for spark 2.1.0 build. Depend on spark 2.1.0.

--- ci/install-spark.sh
+++ ci/install-spark.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -o /tmp/spark.tgz http://apache.mirror.anlx.net/spark/spark-2.0.2/spark-2.0.2-bin-hadoop2.6.tgz
+curl -o /tmp/spark.tgz http://apache.mirror.anlx.net/spark/spark-2.1.0/spark-2.1.0-bin-hadoop2.6.tgz
 tar -xvzf /tmp/spark.tgz -C /tmp
