6868fdcc97b8996499c36379d1ceed586da3baf1
barrybecker <bbe@esi-group.com>
2018-04-25 07:12:37 -0700

    MIN-7288, MIN-7360, MIN-7285, MIN-5730 Merge changes from upstream. Build with spark 2.3.0.

--- job-server/config/local.sh
+++ job-server/config/local.sh
@@ -12,11 +12,11 @@ INSTALL_DIR=/home/spark/job-server
 LOG_DIR=/var/log/job-server
 PIDFILE=spark-jobserver.pid
 JOBSERVER_MEMORY=5G
-SPARK_VERSION=2.3.1-SNAPSHOT
+SPARK_VERSION=2.3.0
 MAX_DIRECT_MEMORY=1G
 SPARK_CONF_DIR=$SPARK_HOME/conf
 # Only needed for Mesos deploys
-SPARK_EXECUTOR_URI=$SPARK_HOME/spark-2.3.1-SNAPSHOT.tar.gz
+SPARK_EXECUTOR_URI=$SPARK_HOME/spark-2.3.0.tar.gz
 # Only needed for YARN running outside of the cluster
 # You will need to COPY these files from your cluster to the remote machine
 # Normally these are kept on the cluster in /etc/hadoop/conf
